32d2490bf459da9fa545a1d533892d1f5cd63d71
ZLM <2113743321@qq.com>
2021-07-14 21:42:39 +0800

    完成js

---
+++ index.js
@@ -0,0 +1,19 @@
+const hourHand = document.querySelector('.hour-hand')
+const minHand = document.querySelector('.min-hand')
+const secondHand = document.querySelector('.second-hand')
+
+
+function rotate() {
+    const date = new Date()
+    const hourDeg = date.getHours() * 360 / 12 + date.getMinutes() / 60 * 30 + 90
+    const minDeg = date.getMinutes() * 360 / 60 + date.getSeconds() / 60 * 6 + 90
+    const secondDeg = date.getSeconds() * 360 / 60 + 90
+
+    hourHand.style.transform = `rotate(${hourDeg}deg)`
+    minHand.style.transform = `rotate(${minDeg}deg)`
+    secondHand.style.transform = `rotate(${secondDeg}deg)`
+
+}
+
+setInterval(rotate, 1000);
+rotate()
